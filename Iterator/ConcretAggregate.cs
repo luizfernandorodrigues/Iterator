3eefbe5b285721e6dd26efdc085a0661977835e3
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 18:59:25 -0300

    Add project files.

---
+++ Iterator/ConcretAggregate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    public class ConcretAggregate : Aggreate
+    {
+        private ArrayList itens = new ArrayList();
+        public override Iterator CreateIterator()
+        {
+            return new ConcreteIterator(this);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return itens.Count;
+            }
+        }
+
+        public object this[int index]
+        {
+            get { return itens[index]; }
+            set { itens.Insert(index, value); }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcretAggregate a = new ConcretAggregate();
+
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
+
+            Iterator i = a.CreateIterator();
+
+            Console.WriteLine("Iteragindo com a coleção: ");
+
+            object item = i.First();
+
+            while(item != null)
+            {
+                Console.WriteLine(item);
+
+                item = i.Next();
+            }
+            Console.ReadKey();
+        }
+    }
+}
